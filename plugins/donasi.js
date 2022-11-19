81abe7124b8acf039176bfbce585aad54e9234c7
ditya-astawa <115558329+ditya-astawa@users.noreply.github.com>
2022-11-19 08:05:55 +0800

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-            .✵.𝗚𝗥𝗨𝗣 𝗣𝗘𝗡𝗧𝗔𝗕𝗢𝗧.✵.
+            .✵.GRUP DITYABOTZ.✵.
 
-            https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2
+            https://chat.whatsapp.com/Ej8JuTdjGnA4XxLGbwlpMP
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
